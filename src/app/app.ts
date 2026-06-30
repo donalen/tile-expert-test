052d344bc0f654806a55381095eff5d2046c4ffd
Alena Demchenko <alenademich@gmail.com>
2026-06-30 15:08:10 +0200

    Implement header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.scss'
+// })
+// export class App {
+//   protected readonly title = signal('remote-team-test');
+// }
+
+
+import { Component } from '@angular/core';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('remote-team-test');
-}
+export class App {}
---
+++ src/app/header/header.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faSearch,
+  faCirclePlus,
+  faBell,
+  faLink,
+  faUsers,
+  faHashtag,
+  faStar,
+  faClockRotateLeft,
+  faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, FontAwesomeModule],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  isSearchOpen = false;
+
+  faSearch = faSearch;
+  faCirclePlus = faCirclePlus;
+  faBell = faBell;
+  faLink = faLink;
+  faUsers = faUsers;
+  faHashtag = faHashtag;
+  faStar = faStar;
+  faClockRotateLeft = faClockRotateLeft;
+  faArrowLeft = faArrowLeft;
+
+  openSearch(): void {
+    this.isSearchOpen = true;
+  }
+
+  closeSearch(): void {
+    this.isSearchOpen = false;
+  }
+}
