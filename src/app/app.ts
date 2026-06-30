2d11bbb238e26ca356f1e437d4627882cbf1e166
Alena Demchenko <alenademich@gmail.com>
2026-06-30 16:31:55 +0200

    Remove commented lines

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,3 @@
-// import { Component, signal } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   imports: [RouterOutlet],
-//   templateUrl: './app.html',
-//   styleUrl: './app.scss'
-// })
-// export class App {
-//   protected readonly title = signal('remote-team-test');
-// }
-
-
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 
